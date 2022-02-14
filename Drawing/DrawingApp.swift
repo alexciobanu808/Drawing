f724c1afd0dce5f6268b90f3ee5de81731da57fe
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2022-02-14 13:54:14 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Alex Ciobanu on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
